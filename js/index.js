815f3fb6ab68e020961d1cfbada82b69cab800c6
thaonguyen261 <105218140+thaonguyen261@users.noreply.github.com>
2022-05-16 20:06:38 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Assign variables 
+let first_name = 'Thao';
+let last_name = 'Nguyen Hoang';
+let email = 'nguy0986@algonquinlive.com'; 
+let output = 'My name is ' + first_name + ' ' + last_name + ' .You can contact me at ' + email +'.';
+
+
+// Alert output
+
+alert(output);
